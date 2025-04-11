b48d09f8b0ea71a80b9ae08a9edb8e6e4f1119b3
Ahana416 <ahanaagashe@gmail.com>
2025-04-11 06:30:02 +0530

    completed project

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,24 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import productRouter from './routes/product.router.js';
-// import path from 'path';
+import path from 'path';
 
 dotenv.config(); 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use('/api/products', productRouter);
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname, '/frontend/dist')));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-//     })
-// }
-// console.log(process.env.MONGO_URI);
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+}
+console.log(process.env.MONGO_URI);
 app.listen(PORT, () => {
     connectDB();
     console.log('Server started at http://localhost:'+PORT);
